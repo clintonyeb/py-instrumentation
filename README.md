5c995c47af1732074dbef441c322113e85b42ff7
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-08 15:15:54 -0600

    added readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-https://bitbucket.org/jsbueno/metapython/src/default/
+# Python Instrumentation
+
+This module is a proof of concept that python modules/files can be weaved.
+
+The main file doing the heavy work is: `aspect.py`
+It exports one function called weave with a module has to call with its name.
+
+For example: To weave a Python module:
+
+1. Import weave from the aspect module: `from aspect import weave`
+2. Call the weave method providing the name of the module you want to weave, in this case the current module.
+Example: `weave(sys.modules[__name__])`
+
+How To Run:
+
+1. The project includes a Test bed named `test.py`
+2. You can simply run it by calling `python test.py`
+3. It will produce a log file: `method-calls.log` which will contain the method calls during its execution.
+
+Future Work:
+1. Make the project pip installable. This way projects can easily use it.
--- aspect.py
+++ aspect.py
@@ -10,16 +10,16 @@ logging.basicConfig(filename='method-calls.log', level=logging.DEBUG, format=FOR
 
 @aspectlib.Aspect(bind=True)
 def _log_function(cut_point, *args, **kwargs):
-    log.debug("`%s` called with args: %s kwargs: %s" % (cut_point.__qualname__, args, kwargs))
+    log.debug("'%s' called with args: %s kwargs: %s" % (cut_point.__qualname__, args, kwargs))
 
     try:
         yield
-        log.debug("`%s` returned successfully..." % (cut_point.__qualname__,))
+        log.debug("'%s' returned successfully..." % (cut_point.__qualname__,))
     except Exception as exc:
-        print("Exception raised in `%s` %r for %s/%s" % (cut_point.__qualname__, exc, args, kwargs))
-        raise
+        print("Exception raised in '%s' %r for %s/%s" % (cut_point.__qualname__, exc, args, kwargs))
+        raise exc
 
 
 def weave(module_name):
-    components = inspect.getmembers(module_name, lambda x: inspect.isfunction(x) or x == inspect.isclass(x))
+    components = inspect.getmembers(module_name, lambda x: inspect.isfunction(x) or inspect.isclass(x))
     [aspectlib.weave(obj, _log_function) for name, obj in components]
